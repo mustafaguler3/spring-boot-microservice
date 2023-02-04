4bd5a7645eae2a317f5c41d24cafcc7ca7af2bd5
mustafaguler3 <mustafa.glr@hotmail.com>
2023-02-04 20:01:51 +0300

    added employee microservice

---
+++ employee-service/src/main/java/com/example/demo/entity/Employee.java
@@ -0,0 +1,23 @@
+package com.example.demo.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "employees")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+}
